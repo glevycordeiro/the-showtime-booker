fb76945aa616d289b3c170893815ace1312f2221
Mariana Mendes <mariana.omendes@gmail.com>
2020-02-11 21:49:03 +0000

    created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,66 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Creating 10 random users...'
+
+10.times do |user|
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    dob: Faker::Number.between(from: 18, to: 200),
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    avatar: Faker::Avatar.image
+  )
+  user.save!
+end
+
+movie_1917 = Movie.create([{ title: '1917', synopsis: 'lorem', genre: 'drama', duration: '120 minutes', poster: 'https://media-cache.cinematerial.com/p/500x/haeorwgk/1917-british-movie-poster.jpg?v=1579166770', age_limit: 16, imdb_url: 'https://www.imdb.com/title/tt8579674/', cinema: cinema.all.sample(1)}])
+movie_parasite = Movie.create([{ title: 'Parasite', synopsis: 'lorem', genre: 'comedy', duration: '90 minutes', poster: 'https://fleurcinema.com/filesystem/fleur-cinema/Movies/2019/November/parasite.png', age_limit: 18, imdb_url: 'https://www.imdb.com/title/tt6751668/', cinema: cinema.all.sample(1)}])
+movie_judy = Movie.create([{ title: 'Judy', synopsis: 'lorem', genre: 'musical', duration: '110 minutes', poster: 'https://m.media-amazon.com/images/M/MV5BYmE0OTE5NWItMGYyZi00MzUxLWFjN2QtYzBkZGRjZGVmMGFmXkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_SY999_SX648_AL_.jpg', age_limit: 14, imdb_url: 'https://www.imdb.com/title/tt7549996/', cinema: cinema.all.sample(1)}])
+
+cinema_colombo = Cinema.create([{ user: user.all.sample(1), address: 'Avenida Lusíada', name: 'Colombo'}])
+cinema_amoreiras = Cinema.create([{ address: 'Av Engenheiro Duarte Pacheco', user: user.all.sample(1), name: 'Amoreiras'}])
+cinema_uci = Cinema.create([{ address: 'São Sebastião', user: user.all.sample(1), name: 'El Corte Inglês'}])
+cinema_cascais = Cinema.create([{ address: 'Cascais', user: user.all.sample(1), name: 'Cascais Shopping'}])
+cinema_almada = Cinema.create([{ address: 'Pragal, Almada', user: user.all.sample(1), name: 'Almada Fórum'}])
+cinema_montijo = Cinema.create([{ address: 'Montijo', user: user.all.sample(1), name: 'Fórum Montijo'}])
+
+puts 'Creating movie_sessions...'
+
+10.times do |movie_session|
+  movie_session = Movie_session.new(
+    start_date_time: Faker::Number.decimal(l_digits: 2).between(from: '00:00', to: '24:00'),
+    end_date_time: Faker::Number.decimal(l_digits: 2).between(from: '00:00', to: '24:00'),
+    movie_id: movie.all.sample(1),
+    capacity: Faker::Number
+  )
+  movie_session.save!
+end
+
+puts 'Creating 10 random bookings...'
+
+10.times do |booking|
+  booking = Booking.new(
+    user: user.all.sample(1),
+    status: Faker::Boolean.boolean,
+    movie_session_id: movie_session.all.sample(1)
+  )
+  booking.save!
+end
+
+puts 'Creating 10 random reviews...'
+
+10.times do |review|
+  review = Review.new(
+    user: user.all.sample(1),
+    date: Faker::Date,
+    description: Faker::Lorem.paragraph,
+    rating: Faker::Number.between(from: 0, to: 10),
+    booking_id: booking.all.sample(1)
+  )
+  review.save!
+end
