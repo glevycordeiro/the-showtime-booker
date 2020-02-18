8fbef5df03dd5b28a8cb3ad1155cc1d6668750f9
nuno tobias <nmtobias@gmail.com>
2020-02-18 20:30:38 +0000

    added policys to movie session

--- app/policies/movie_session_policy.rb
+++ app/policies/movie_session_policy.rb
@@ -4,4 +4,16 @@ class MovieSessionPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def destroy?
+    true
+  end
 end
