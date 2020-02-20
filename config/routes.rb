aa9a593b141335dc217694ff1b98363188e4b025
Mariana Mendes <mariana.omendes@gmail.com>
2020-02-20 20:31:22 +0000

    booking routes changed and booking added to movies controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :bookings, only: [:new,:create]
   resources :movies, only: [:index, :show,:destroy]
   resources :cinemas do
     resources :movies, only: [:new, :create] do
       resources :movie_sessions, only: [:new, :create, :edit, :update] do
-        resources :bookings, only: [:new,:create]
+
       end
     end
   end
