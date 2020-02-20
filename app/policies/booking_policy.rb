9f79dfd571f5c79a175ce9dca5e5aee89c7c70f5
nuno tobias <nmtobias@gmail.com>
2020-02-20 19:25:03 +0000

    booking policy done

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -4,4 +4,18 @@ class BookingPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  # index, show, new, create, edit update
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
 end
