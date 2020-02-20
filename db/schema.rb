0b4f8a8826e861b33dad56f74d10b3504bdc2c85
Goncalo Cordeiro <glevycordeiro@gmail.com>
2020-02-20 20:01:40 +0000

    added price to both movie sessions and bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_194119) do
+ActiveRecord::Schema.define(version: 2020_02_20_200021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_02_18_194119) do
     t.bigint "movie_session_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price"
     t.index ["movie_session_id"], name: "index_bookings_on_movie_session_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
@@ -64,6 +65,7 @@ ActiveRecord::Schema.define(version: 2020_02_18_194119) do
     t.datetime "updated_at", null: false
     t.boolean "status"
     t.time "start_time"
+    t.integer "price"
     t.index ["movie_id"], name: "index_movie_sessions_on_movie_id"
   end
 
