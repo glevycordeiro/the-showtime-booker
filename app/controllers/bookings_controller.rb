c93dc58207b4fdfe222c73debac4250c11381e77
Goncalo Cordeiro <glevycordeiro@gmail.com>
2020-02-20 19:40:02 +0000

    finished controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,21 +1,37 @@
 class BookingsController < ApplicationController
+  before_action :check_if_reedem
   def new
     @booking = Booking.new
     @booking.status = "active"
   end
 
   def create
-    if @booking.status == "redeemed"
-      @booking = Booking.new(booking_params)
-      @booking.user = current_user
-      @booking.save
-      redirect_to bookings_path(@user)
-    end
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.save
+    redirect_to bookings_path(@user)
+  end
+
+  def edit
+    @bookings = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.status = "cancelled"
   end
 
   private
 
   def booking_params
     params.require(:booking).permit(:user, :status, :movie_session)
+  end
+
+  def check_if_redeem
+    Booking.all.each do |booking|
+      if booking.start_date > Y
+        @booking.status = "redeemed"
+      end
+    end
   end
 end
