b6da9cdca90890f498d71608f89485de13b6ddea
Frederic Baugnee <fredericbaugnee@gmail.com>
2020-02-22 12:08:11 +0000

    image javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,23 @@
 import "bootstrap";
 import "plugins/flatpickr"
+
+
+
+$(function() {
+  function readURL(input) {
+
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $('#img_prev').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  $("#user_avatar").change(function(){
+    $('#img_prev').removeClass('hidden');
+    readURL(this);
+  });
+});
