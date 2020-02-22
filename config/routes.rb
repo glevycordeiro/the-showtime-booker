1bef6b38d400a1e0bc95733947a04b76df348271
Frederic Baugnee <fredericbaugnee@gmail.com>
2020-02-22 13:34:57 +0000

    booking show route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :bookings, only: [:new,:create]
   resources :movies, only: [:index, :show,:destroy]
-  resources :bookings, only: [:new, :create, :edit, :update]
+  resources :bookings, only: [:new, :create, :edit, :update,:show]
   resources :cinemas do
     resources :movies, only: [:new, :create] do
       resources :movie_sessions, only: [:new, :create, :edit, :update]
